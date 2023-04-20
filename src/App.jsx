c4f91434137b302da855db3c5e8ba0a4517e6767
Romina.Sosa <romijunio@hotmail.com>
2023-04-20 02:53:15 -0300

    Se borra firebaseprovider

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,19 @@ import { getFirestore } from "firebase/firestore";
 import React from "react";
 
 import { BrowserRouter } from "react-router-dom";
-import { FirestoreProvider, useFirebaseApp } from "reactfire";
 import "./App.scss";
 import Router from "./Router";
 import Footer from "./components/Footer";
 import NavBarMenu from "./components/NavBar";
 
 function App() {
-  const firestoreInstance = getFirestore(useFirebaseApp());
 
   return (
     <React.StrictMode>
       <BrowserRouter>
-        <FirestoreProvider sdk={firestoreInstance}>
           <NavBarMenu />
           <Router />
           <Footer />
-        </FirestoreProvider>
       </BrowserRouter>
     </React.StrictMode>
   );
--- src/index.js
+++ src/index.js
@@ -3,29 +3,17 @@ import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import "react-multi-carousel/lib/styles.css";
 import "react-notifications/lib/notifications.css";
-import { FirebaseAppProvider } from "reactfire";
 import App from "./App";
 import "./index.scss";
 import reportWebVitals from "./reportWebVitals";
 
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
       <Suspense fallback={<p>Cargando...</p>}>
         <App />
       </Suspense>
-    </FirebaseAppProvider>
   </React.StrictMode>
 );
 
